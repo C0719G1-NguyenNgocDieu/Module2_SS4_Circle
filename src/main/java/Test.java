7894f5cecc3ba1999cd53371a179d30d1e5f5f87
nndieu <nndieu88@gmail.com>
2019-09-12 14:53:27 +0700

    SS4

---
+++ src/main/java/Test.java
@@ -0,0 +1,9 @@
+public class Test {
+    public static void main(String[] args) {
+        Circle circle = new Circle(3.0, "red");
+        System.out.println(circle);
+
+        Cylinder cylinder = new Cylinder(3.0, 5, "yellow");
+        System.out.println(cylinder);
+    }
+}
